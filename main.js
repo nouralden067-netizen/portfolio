04287abe4d410d104617ebb52ccae36f06f7f9c2
Nour alden Said <nouralden067@gmail.com>
2025-11-02 21:17:46 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    menuToggle.querySelector("i").classList.toggle("fa-xmark");
+});
